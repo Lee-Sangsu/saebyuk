0ba3dc72595bc0faf2f4df538c867207cd17233a
Lee-Sangsu <63631278+Lee-Sangsu@users.noreply.github.com>
2020-12-29 02:50:22 +0900

    docker production
    but there was nginx upstream error

---
+++ app/saebyuk/seabyuk/book/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from ..models import Book, UserModel, BookInfo, BorrowBooks, BookComment, RequestedBook, RecommendedBook
+
+
+class BorrowedBooksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: BorrowBooks
+        fields = ['book', 'borrowed_at', 'is_overdue']
+
+
+class BookInfoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = BookInfo
+        fields = '__all__'
+
+
+class MainBookSerializer(serializers.ModelSerializer):
+    book_info = BookInfoSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Book
+        fields = ['isbn', 'registered_date', 'book_info']
